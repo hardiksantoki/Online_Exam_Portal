40c3b3d9933ff50304e4c671afa4015061c9ccdc
Hardik Santoki <65931558+hardiksantoki@users.noreply.github.com>
2022-05-18 16:47:26 +0530

    update databaseconnection

--- databaseCon.js
+++ databaseCon.js
@@ -3,13 +3,9 @@ const client = new Client({
     user: "postgres",
     host: "localhost",
     database: "ExamR",
-    password: "hardik@007",
+    password: "1234",
     port: 5432,
-    // user: 'vskcisevzglzdk',
-    // host: 'ec2-34-231-63-30.compute-1.amazonaws.com',
-    // database: 'dfv716h3lo3jhf',
-    // password: '3b2fe11ba5be1458693c93f4529cf068f4ec5ccb22746824957413372386eadd',
-    // port: 5432,
+   
 })
 
 client.on("connect", () => {
